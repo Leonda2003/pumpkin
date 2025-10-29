def2c8a594b2eda66bde28845b98ac570e1f38de
Leonda2003 <mikitenghi@gmail.com>
2025-10-29 12:40:09 +0100

    v

---
+++ src/test/java/com/example/pumpkin/PumpkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pumpkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
